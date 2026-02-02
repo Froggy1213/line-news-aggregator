fe4431a57b1b5dd34833571929eb6c9cdaec4b41
Froggy1213 <minimausnik@gmail.com>
2026-02-02 10:09:52 +0900

    MVP ver 0.1

--- app/config.py
+++ app/config.py
@@ -3,6 +3,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     PROJECT_NAME: str = "Line News Aggregator"
     DATABASE_URL: str
+    
+    # Добавляем новые настройки:
+    LINE_CHANNEL_SECRET: str
+    LINE_CHANNEL_ACCESS_TOKEN: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
